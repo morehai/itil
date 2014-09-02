0391b4822c4c503830731bc9f12113afa253a805
anuyang <anuyang@10.8.201.184>
2014-09-02 11:57:32 +0800

    同步新代码

--- src/com/wao/itil/service/TaskManager.java
+++ src/com/wao/itil/service/TaskManager.java
@@ -9,7 +9,7 @@ import com.wao.itil.model.Task;
 public interface TaskManager extends BaseManager<Task> {
 
 	/**
-	 * 默认每次调用返回5条需要更新的任务
+	 * 默认每次调用返回500条需要更新的任务
 	 * @return
 	 */
 	public List<Task> findByUnExecuted();
